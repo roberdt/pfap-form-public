f6f70fe2c94eef5cefb42d4ff9053d52bcc94a8d
Robert Erdt <rerdt@safetynetaccess.info>
2025-10-03 14:12:52 -0400

    Added new functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import './css/styles.css';
+import {ComponentPreviews, useInitial} from "../components/dev";
+import {DevSupport} from "@react-buddy/ide-toolbox-next";
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return <DevSupport ComponentPreviews={ComponentPreviews}
+                       useInitialHook={useInitial}
+    >
+        <Component {...pageProps} />
+    </DevSupport>
 }
 
 export default MyApp;
